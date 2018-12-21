f473ef25b88d13a366ba510113493eca21c0fe11
lschiesser <41335130+lschiesser@users.noreply.github.com>
2018-12-21 13:49:07 +0100

    Testing out gui programming using tkinter

--- gui.py
+++ gui.py
@@ -1,1 +1,16 @@
+from tkinter import *
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.button_quit = Button(text="Quit", command=frame.quit)
+        self.button_quit.pack(side=LEFT)
+        self.button_execute = Button(text="Execute search", command=self.execute)
+        self.button_execute.pack(side=LEFT)
 
+    def execute(self):
+        print("Hi")
+
+root = Tk()
+app = App(root)
+root.mainloop()
