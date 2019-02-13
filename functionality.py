df198036014312832beb5d4947d1b609ca564cbb
lschiesser <41335130+lschiesser@users.noreply.github.com>
2019-02-13 09:37:30 +0100

    Update functionality.py

--- functionality.py
+++ functionality.py
@@ -2,4 +2,17 @@
 
 # first method: receives all information from gui and begins with Preprocessing
 def preprocess_info(warehouse, order, algorithm, value_alg):
+    # preprocess information
+    # after preprocessing pass neighborhood on to search algorithm
+    if algorithm == 1:
+        #hill Climbing
+    elif algorithm == 2:
+        #first choice hill Climbing
+    elif algorithm == 3:
+        #parallel hill Climbing
+    elif algorithm == 4:
+        #simulated Annealing
+    elif algorithm == 5:
+        #local beam search
+
     return
