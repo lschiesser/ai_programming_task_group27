92bb9ab1fbd13483ede696840fcfba0f11ac5459
Berit19 <46372060+Berit19@users.noreply.github.com>
2019-02-22 12:17:52 +0100

    Update search_algorithms.py

--- search_algorithms.py
+++ search_algorithms.py
@@ -15,3 +15,35 @@ def simaneal(neighborhood, t):
         elif math.exp(delta)/t >= random.randrange(0, 1):
             current = next
         t = t - 1
+
+def hillclimbing(gradedPSUs):
+    neighbor = 0
+    current = random.randint(0, len(gradedPSUs) - 1)
+    if gradedPSUs[current + 1] > gradedPSUs[current - 1]:
+        neighbor = current + 1
+    else:
+        neighbor = current - 1
+
+    while gradedPSUs[current] < gradedPSUs[neighbor]:
+        current = neighbor
+        if gradedPSUs[current + 1] > gradedPSUs[current - 1]:
+            neighbor = current + 1
+        else:
+            neighbor = current - 1
+    return current
+
+
+def firstchoicehc(gradedPSUs):
+    neighbor = 0
+    current = random.randint(0, len(gradedPSUs) - 1)
+    if gradedPSUs[current + 1] > gradedPSUs[current - 1]:
+        neighbor = current + 1
+    else:
+        neighbor = current - 1
+
+    while gradedPSUs[current] < gradedPSUs[neighbor]:
+        if gradedPSUs[current + 1] > gradedPSUs[current]:
+            current = current + 1
+        elif gradedPSUs[current - 1] > gradedPSUs[current]:
+            current = current - 1
+    return current
